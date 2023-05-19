2e8ed773253bab455928019d8ca12b4c7d20ca07
Jimmy Clairy <jimcl@hotmail.fr>
2023-05-19 19:59:01 +0200

    Add header.scss

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('Connected');
+const header = document.querySelector('.header__overlay')
+window.addEventListener('scroll', () => {
+    // console.log(window.scrollY);
+    if (window.scrollY > 50) {
+        header.classList.add("active")
+    } else {
+        header.classList.remove("active")
+    }
+})
