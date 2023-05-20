c55534b1d8a6ecd1cf84e23a2830e66eaf05f4ab
Jimmy Clairy <jimcl@hotmail.fr>
2023-05-20 15:31:12 +0200

    Add btn-toogle and nav-slider

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ window.addEventListener('scroll', () => {
         header.classList.remove("active")
     }
 })
+
+// Effect nav-slider ---------------------------------------------------------------------------------------------
+const btnToggle = document.querySelector(".btn-toggle")
+const navSlider = document.querySelector(".nav-slider")
+const navSliderLink = document.querySelectorAll(".nav-slider__link")
+
+btnToggle.addEventListener("click", toggleNav)
+navSliderLink.forEach(a => a.addEventListener("click", () => toggleNav()))
+
+function toggleNav() {
+    btnToggle.classList.toggle("active")
+    navSlider.classList.toggle("active")
+}
